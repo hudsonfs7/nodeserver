ba34ced632bdbadf2f6f6dc3abc43350360fcfb3
Hudson Souza <hudsonfs7@hotmail.com>
2023-08-17 00:05:26 -0300

    Express e nodemon configurados

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-const app = require('express')
+const express = require('express')
 
-app.listem(4002, () => console.log('Server rodando!'))
+const app = express()
+
+app.get('/primeira-rota', (req, res) => {
+  return res.json({
+    message: 'Primeira Rota do express'
+  })
+})
+app.listen(4002, () => console.log('Nodemon rodando!'))
