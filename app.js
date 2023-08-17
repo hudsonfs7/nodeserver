8f25dc01670846fdf7e37c772f8dd75c6a65425e
Hudson Souza <hudsonfs7@hotmail.com>
2023-08-17 18:04:13 -0300

    Primeira rota criada, a de produtos, incluindo as informações de name, price e id

--- app.js
+++ app.js
@@ -1,10 +1,24 @@
 const express = require('express')
-
+const { randomUUID } = require('crypto')
 const app = express()
 
-app.get('/primeira-rota', (req, res) => {
-  return res.json({
-    message: 'Primeira Rota do express'
-  })
+app.use(express.json())
+
+const products = []
+
+// Post =>
+app.post('/products', (req, res) => {
+  const { name, price } = req.body
+
+  const product = {
+    name,
+    price,
+    id: randomUUID()
+  }
+
+  products.push({ product })
+
+  return res.json(product)
 })
+
 app.listen(4002, () => console.log('Nodemon rodando!'))
